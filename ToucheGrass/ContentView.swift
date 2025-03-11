89b980e7bfd790857e27202629d91c1f35a0c5d8
huguestrade <hugues.estrade@edu.univ-fcomte.fr>
2025-03-11 08:12:18 +0100

    Initial Commit

---
+++ ToucheGrass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToucheGrass
+//
+//  Created by estrade hugues on 18/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToucheGrass/ToucheGrassApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToucheGrassApp.swift
+//  ToucheGrass
+//
+//  Created by estrade hugues on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToucheGrassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
